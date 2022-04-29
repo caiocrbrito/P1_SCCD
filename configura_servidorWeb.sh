5a36008177489aef349b7451089629c314263e28
“caiocrbrito” <caiocrbrito@gmail.com>
2022-04-29 00:26:02 +0000

    primeiro commit

---
+++ configura_servidorWeb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Atualizando os pacotes
+apt-get update
+apt-get upgrade
+
+# Instalando os pacotes apache, wget, zip e unzip
+apt install apache2
+apt install wget
+apt install zip unzip
+apt install git-all
+
+# Fazendo download do arquivo
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+# Descompactando o arquivo na pasta apache2
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip  -d /var/www/html
+
+# Deletando arquivo index
+rm /var/www/html/index.html
+
+# Enviando o arquivo para o servidor
+mv /var/www/html/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/cv-completo.html /var/www/html/cv-completo.html
+mv /var/www/html/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/style.css /var/www/html/style.css
+mv /var/www/html/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/cv-resumido.html /var/www/html/index.html
+
+# Removendo pasta
+rm -r /var/www/html/873219-690e1f604dc8ead2583589e1aee6f8a42040a035
+
